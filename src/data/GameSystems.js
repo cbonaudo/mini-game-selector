6505be8e72e2b18a5952745ab675481a407d6462
Arlune <morgane.gaillard69@gmail.com>
2020-01-16 15:12:37 +0100

    Add SubSystems & change Codes

--- src/data/GameSystems.js
+++ src/data/GameSystems.js
@@ -1,27 +1,27 @@
 export const GameSystems = [
   {
     name: "Age Of Sigmar",
-    code: "AS",
+    code: "AOS",
     description: "Games Workshop's official Fantasy miniatures wargame",
   },
   {
     name: "Warhammer 40.000",
-    code: "WK",
+    code: "WAK",
     description: "Games Workshop's official Sci-Fi miniatures wargame",
   },
   {
     name: "Age Of Fantasy",
-    code: "AF",
+    code: "AOF",
     description: "One Page Rules spin for Fantasy miniatures wargaming",
   },
   {
     name: "Grimdark Future",
-    code: "GF",
+    code: "GRF",
     description: "One Page Rules spin on Sci-Fi miniatures wargaming",
   },
   {
     name: "One-Off Games",
-    code: "OG",
+    code: "OOG",
     description: "One Page Rules miscellaneous miniatures games",
   },
 ];
---
+++ src/data/SubSystems.js
@@ -0,0 +1,29 @@
+export const SubSystems = [
+    {
+        name: "Skirmish",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        code: "SKI",
+        gameSystemCode: "AOS",
+    },
+
+    {
+        name: "Skirmish",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        code: "SKI",
+        gameSystemCode: "AOF",
+    },
+
+    {
+        name: "Normal",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        code: "NRM",
+        gameSystemCode: "WAK",
+    },
+
+    {
+        name: "Apocalypse",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        code: "APO",
+        gameSystemCode: "OOG",
+    },
+];
